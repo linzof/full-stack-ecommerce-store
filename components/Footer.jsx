60a4fdcda929935f5f941838bb4bdda4eb037b33
chrislinzLBX <chris@lnz.digital>
2022-07-18 06:48:36 -0700

    fixed placeholder text

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,15 +1,15 @@
-import React from 'react'
-import { AiFillInstagram, AiOutlineTwitter } from 'react-icons/ai'
+import React from "react";
+import { AiFillInstagram, AiOutlineTwitter } from "react-icons/ai";
 const Footer = () => {
   return (
     <div className="footer-container">
-      <p>2022 JSM Headphone All rights reserved</p>
+      <p>2022 | Da Drip | All rights reserved</p>
       <p className="icons">
-        <AiFillInstagram/>
-        <AiOutlineTwitter/>
+        <AiFillInstagram />
+        <AiOutlineTwitter />
       </p>
     </div>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
