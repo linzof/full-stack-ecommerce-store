f37866b2ef2aa013526d28aaef05c0cb7094a9ec
chrislinzLBX <chris@lnz.digital>
2022-07-17 18:18:47 -0700

    initial commit

---
+++ components/HeroBanner.jsx
@@ -0,0 +1,35 @@
+import Link from "next/link";
+import React from "react";
+
+import { urlFor } from "../lib/client";
+
+const HeroBanner = ({ heroBanner }) => {
+  return (
+    <div className="hero-banner-container">
+      <div className="hero-content-container">
+        <p className="beats-solo">{heroBanner.smallText}</p>
+        <h3>
+          {heroBanner.largeText1} {heroBanner.largeText2}
+        </h3>
+        <h1>HOT</h1>
+        <img
+          src={urlFor(heroBanner.image)}
+          alt="headphones"
+          className="hero-banner-image"
+        />
+
+        <div>
+          <Link href={`/product/${heroBanner.product}`}>
+            <button type="button">{heroBanner.buttonText}</button>
+          </Link>
+          <div className="desc">
+            <h5>Description</h5>
+            <p>{heroBanner.desc}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HeroBanner;
